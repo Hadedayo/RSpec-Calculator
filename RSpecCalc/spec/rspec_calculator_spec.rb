799d3541a10408548635603a39c1303022145a5d
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-11-20 15:12:53 +0000

    Created tests for each def method created

--- RSpecCalc/spec/rspec_calculator_spec.rb
+++ RSpecCalc/spec/rspec_calculator_spec.rb
@@ -2,7 +2,25 @@ require "spec_helper" #USE require for a gem
 #USE require-RELATIVE FOR files
 
 describe "CalcEngine" do
-  it "should correctly add two numbers" do
 
+  before(:each) do
+    @calc = CalcEngine.new
   end
+
+  it "should correctly add two numbers" do
+    expect(@calc.add(1,2)).to eq 3
+  end
+
+  it "should correctly subtract a number from another" do
+    expect(@calc.subtract(14,8)).to eq 6
+  end
+
+  it "should multiply two numbers correctly" do
+    expect(@calc.multiply(9,8)).to eq 72
+  end
+
+  it "should divide a number from another correctly" do
+    expect(@calc.divide(9,3)).to eq 3
+  end
+
 end
